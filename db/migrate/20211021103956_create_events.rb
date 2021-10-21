d657b69db5062d3d3236ee31ef55dfbce9087449
Shresth Khede <shresthyuvasoft332@gmail.com>
2021-10-21 17:49:26 +0530

    Initial commit

---
+++ db/migrate/20211021103956_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :event_type
+      t.boolean :public
+      t.integer :repo_id
+      t.integer :actor_id
+
+      t.timestamps
+    end
+  end
+end
